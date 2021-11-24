1f3fa84a84b2e5c159ff14ee3c11b88e84710b08
Diego O. da Silva <doliveira.silva28@gmail.com>
2021-11-23 23:21:57 -0300

    Criação da classe Serie.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Series.Classe;
 
 namespace Series
 {
@@ -10,6 +11,7 @@ namespace Series
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
         }
     }
 }
