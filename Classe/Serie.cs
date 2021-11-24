aa0668a9add5f8960bbdd546dba6575f8d9b5dd0
Diego O. da Silva <doliveira.silva28@gmail.com>
2021-11-23 23:32:42 -0300

    Definição dos atributos da classe Serie e criação do construtor. Criação do arquivo enum com generos dos filmes.

--- Classe/Serie.cs
+++ Classe/Serie.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Series.Enum;
 
 namespace Series.Classe
 {
-    class Serie : EntidadeBase
+    public class Serie : EntidadeBase
     {
+         //Atributos
 
+        private Genero genero { get; set; }
+
+        private string Titulo { get; set; }
+
+        private string Descricao { get; set; }
+
+        private int Ano { get; set; }
+
+        //Métodos
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
     }
 }
---
+++ Enum/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Series.Enum
+{
+   public enum Genero
+    {
+        Acao = 1,
+        Aventura,
+        Comedia,
+        Documentario,
+        Drama,
+        Espionagem,
+        Faroeste,
+        Fantasia,
+        Ficcao_Cientifica,
+        Musical,
+        Romance,
+        Suspense,
+        Terror
+    }
+}
