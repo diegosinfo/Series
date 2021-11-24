0ac66c45ba6b299ce7a3ddec5d9c7a542634b73e
Diego O. da Silva <doliveira.silva28@gmail.com>
2021-11-23 23:54:12 -0300

    Definição dos metodos da IRepositorio

--- Interfaces/IRepositorio.cs
+++ Interfaces/IRepositorio.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Series.Interfaces
 {
-    interface IRepositorio
+    public interface IRepositorio<T>
     {
+        List<T> Lista();
+        T RetornaPorId(int id);
+
+        void Insere(T entidade);
+        
+        void Exclui(int id);
+
+        void Atualiza(int id, T entidade);
+
+        int ProximoId();
+
     }
 }
