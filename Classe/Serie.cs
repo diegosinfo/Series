d9b56e4ed23a5f124ae3a650755cd83bde7bc8d4
Diego O. da Silva <doliveira.silva28@gmail.com>
2021-11-23 23:42:30 -0300

    Alteraçao do nome class1.cs em enum para genero.cs Implementação de metodos na classe Serie.

--- Classe/Serie.cs
+++ Classe/Serie.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Series.Enum;
 
 namespace Series.Classe
@@ -27,6 +23,27 @@ namespace Series.Classe
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano;
+
+            return retorno;
+        }
+
+        public string RetornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int RetornaId()
+        {
+            return this.Id;
         }
     }
 }
